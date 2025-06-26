13f5215a65fcb66093603ca69be6afa7852d3c50
Thanuja Pilla <thanujapilla7@gmail.com>
2025-06-26 06:47:36 +0000

    changed port line code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const mongoURL = process.env.MONGO_URL;
 const dbName = "eventdb";
